881d606351227818c4163fe3dab749cd84ec484b
Leen Haq <leenhaq31@gmail.com>
2026-04-16 01:07:20 +0300

    Initial commit: auth UI (login & register)

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const green = Color(0xFF2E7D32);
+  static const greenDark = Color(0xFF1B5E20);
+  static const greenLight = Color(0xFF4CAF50);
+  static const greenSoft = Color(0xFFEDF7EE);
+  static const background = Color(0xFFF0F4F0);
+  static const white = Color(0xFFFFFFFF);
+  static const textDark = Color(0xFF1A1A2E);
+  static const textGrey = Color(0xFF6B7280);
+  static const textLight = Color(0xFF9CA3AF);
+  static const border = Color(0xFFE5E7EB);
+  static const red = Color(0xFFDC2626);
+  static const orange = Color(0xFFEA580C);
+  static const blue = Color(0xFF1D4ED8);
+  static const purple = Color(0xFF7C3AED);
+  static const teal = Color(0xFF0E7490);
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'core/app_colors.dart';
+import 'screens/login_screen.dart';
+import 'screens/register_screen.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RoadNa',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
+        '/home': (context) => const HomeScreen(),
+      },
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.background,
+        fontFamily: 'Roboto',
+      ),
+    );
+  }
+}
